39ef6faeab3e7c2d6c991ba11b07e0388d877529
Daria <dar.raewa@yandex.ru>
2022-08-11 20:47:17 +0300

    overflow added

--- src/features/MessageList/MessageList.js
+++ src/features/MessageList/MessageList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { USERS } from "../../constants";
 import { Form } from "../Form";
 import { Message } from "../Message";
@@ -8,41 +8,47 @@ import styles from "./MessageList.module.css";
 export const MessageList = () => {
   const [inputValue, setInputValue] = useState("");
   const [data, setData] = useState([]);
+  const scrollAnchor = useRef();
 
   const handleSubmit = () => {
     if (inputValue) {
-      setData([...data, {author: USERS.USER, message: inputValue}])
+      setData([...data, {author: USERS.USER, message: inputValue}]);
+      setInputValue("");
     }
-    setInputValue("");
   };
 
   useEffect(() => {
     let timer;
+    scrollAnchor.current?.scrollIntoView();
+
     if(data.length && data.at(-1).author === USERS.USER) {
       timer = setTimeout(() => {
-        setData([...data, {author: USERS.OPPONENT, message: "Хорошего дня!"}])
-      }, 1500)}
-
-      return () => {
-        clearInterval(timer);
-        window.scrollTo(0, document.body.scrollHeight);
-      };
+        setData([...data, {author: USERS.OPPONENT, message: "Хорошего дня!"}]);
+        scrollAnchor.current?.scrollIntoView();
+      }, 1500)
     }
-  , [data]);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [data]);
     
   return (
     <div className={styles.container}>
-      {
-        data.map((item, index) => 
-          <div key={index}>
-            <Message 
-              user={USERS.USER}
-              author={item.author}
-              message={item.message} 
-            />
-          </div>
-        )
-      }
+      <div className={styles.wrapper}>
+        {
+          data.map((item, index) => 
+            <div key={index}>
+              <Message 
+                user={USERS.USER}
+                author={item.author}
+                message={item.message} 
+              />
+            </div>
+          )
+        }
+        <div ref={scrollAnchor} />
+      </div>
       <Form 
         inputValue={inputValue} 
         setInputValue={setInputValue} 
